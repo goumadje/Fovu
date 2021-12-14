7d6807551ad5b9516d8e05cdd811530a9ebffc06
Georges Rebone <goumadje@yahoo.com>
2021-12-14 11:31:03 -0500

    new commit

--- testing-app.java
+++ testing-app.java
@@ -1,5 +1,29 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  region  = "us-west-1"
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  count         = 2
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+    Env  = "testing"	    
+  }
 }
 
 # Using these data sources allows the configuration to be
